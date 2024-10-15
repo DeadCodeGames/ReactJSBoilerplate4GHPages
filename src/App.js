219d1373851fc8b6ef7351efe64a6957d2ca3612
出口レイ <richard@kanshen.click>
2024-10-15 19:45:47 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import { Router, Routes, Route } from "react-router-dom" - use this for different routes in your web app :3
+import Hewwo from "./components/Hewwo/hewwo.jsx";
+import Tutowial from './components/Tutowial/tutowial.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Hewwo />
+      <Tutowial />
+      <footer>
+        <span>Created in 2024 with </span>
+        <img id="heartemoji" src="https://em-content.zobj.net/source/twitter/154/heavy-black-heart_2764.png" draggable="false" alt="heart emoji" />
+        <span> by kanshen from <span className="x">×</span><a className="deadcode" href="https://deadcode.is-a.dev">DEADCODE</a><span className="x">×</span></span>
+        <span> · </span>
+        <span>{process.env.NODE_ENV === "production" ? (<>clone <a href="https://github.com/DeadCodeGames/ReactJSBoilerplate4GHPages">here</a> :3</>) : "Running in DEVELOPMENT MODE"}</span>
+      </footer>
+    </>
+  );
+}
+
+export default App;
